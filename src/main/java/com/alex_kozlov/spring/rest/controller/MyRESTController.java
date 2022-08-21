9fd4228673305fc4c50009d0b62d18e02a54842a
HtonFirst <al.kozlov.rus@gmail.com>
2022-08-21 11:04:03 +0300

    21.08.2022 final version

--- src/main/java/com/alex_kozlov/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/alex_kozlov/spring/rest/controller/MyRESTController.java
@@ -45,6 +45,21 @@ public class MyRESTController {
 
     }
 
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+
+        Employee employee = employeeService.getEmployee(id);
+
+        if(employee == null) {
+            throw  new NoSuchEmployeeException("There is no employee with ID = "
+                    + id + " in database");
+        }
+
+
+        employeeService.deleteEmployee(id);
+        return  "Employee with ID = " + id + " was deleted";
+    }
+
 
 
 
